f1ff752a86a038483efefd5eac3a92eadb896152
Mohammad Aslam <aslampaasa420@gmail.com>
2025-12-20 02:19:17 +0530

    update code

--- app/index.js
+++ app/index.js
@@ -296,7 +296,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -316,8 +316,9 @@ app.get('/:id', (req, res) => {
     res.send(`hello world ${req.params.id}`)
 });
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
-});
+/* Only listen if not in test */
+if (require.main === module) {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  }
 
 module.exports = app;
